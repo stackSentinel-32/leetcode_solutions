f8c65be8be9da2ab98f0cafc18da41cd89cbd8fc
Piyush Madheshiya <piyushsmmadheshiya08@gmail.com>
2026-03-15 17:53:25 +0530

    Time: 14 ms (43.83%) | Memory: 32.2 MB (49.77%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,14 +1,24 @@
-from collections import Counter
+# from collections import Counter
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         num=Counter(nums)
+#         found=False
+#         for j in num.values():
+#             if j>1:
+#                 found=True
+#                 break
+#             else:
+#                 found=False
+        
+#         return True if found else False
+            
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        num=Counter(nums)
-        found=False
-        for j in num.values():
-            if j>1:
-                found=True
-                break
-            else:
-                found=False
+        hashset=set()
+
+        for i in nums:
+            if i in hashset:
+                return True
+            hashset.add(i)
         
-        return True if found else False
-            
+        return False
