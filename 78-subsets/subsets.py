152fc67a1c524a872f6cd1a39d9c141d19877ff9
Piyush Madheshiya <piyushsmmadheshiya08@gmail.com>
2026-04-01 02:18:46 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (79.84%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n=len(nums)
+        total_subsets=1<<n
+        result=[]
+
+        for num in range(total_subsets):
+            lst=[]
+            for i in range(n):
+                if num & (1<<i) !=0:
+                    lst.append(nums[i])
+            result.append(lst)
+        return result
