e911f781002074fc52e4a3d815a7188fec191ae2
Piyush Madheshiya <piyushsmmadheshiya08@gmail.com>
2026-04-01 02:29:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (79.84%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -12,14 +12,30 @@ class Solution:
         #     result.append(lst)
         # return result
 
-        n=len(nums)
-        total_subsets=1<<n
-        result=[]
+        # n=len(nums)
+        # total_subsets=1<<n
+        # result=[]
 
-        for num in range(total_subsets):
-            lst=[]
-            for i in range(n):
-                if (num>>i)&1 !=0:
-                    lst.append(nums[i])
-            result.append(lst)
+        # for num in range(total_subsets):
+        #     lst=[]
+        #     for i in range(n):
+        #         if (num>>i)&1 !=0:
+        #             lst.append(nums[i])
+        #     result.append(lst)
+        # return result
+
+
+        result=[]
+        
+        def bt(ind,subset):
+            if ind>=len(nums):
+                result.append(subset.copy())
+                return
+            subset.append(nums[ind])
+            bt(ind+1,subset)
+            subset.pop()
+            bt(ind+1,subset)
+
+        bt(0,[])
         return result
+        
