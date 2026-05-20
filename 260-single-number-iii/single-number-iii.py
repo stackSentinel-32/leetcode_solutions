209a1f273c645111993203791740f84c18919a77
Piyush Madheshiya <piyushsmmadheshiya08@gmail.com>
2026-05-20 18:38:05 +0530

    Time: 3 ms (67.35%) | Memory: 21 MB (23.70%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        res=[]
+        indi=0
+        for num in nums:
+            indi^=num
+        
+        diff = indi & -indi
+        a=0
+        b=0
+
+        for x in nums:
+            if x&diff:
+                a^=x
+
+            else:
+                b^=x
+
+        return [a,b]
+
+                                                                                                                                                                                                                                                                                        
