d365d4b7e5faead146aac76749630c2089d5a0d0
Piyush Madheshiya <piyushsmmadheshiya08@gmail.com>
2026-05-28 00:36:39 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (39.89%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,52 +1,28 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-#         num=[]
-#         stack=[]
-#         s=""
-
-#         for i in s:
-#             if i.isdigit():
-#                 num.append(i)
-            
-#             elif i=="[" or i.isalpha():
-#                 stack.append(i)
-            
-#             elif i=="]" and stack!=[]:
-#                 ss=""
-#                 while stack[-1] != "[":
-#                     ss = str(stack[-1]) + ss
-#                     for i in range(num[-1]-1):
-#                         s+=ss
-#                     stack.pop()
-#                 else:
-#                     stack.pop()
-
-#         return s
-
         num=""
         stack=[]
 
         for i in s:
             if i.isdigit():
-                num += i
-
-            elif i == "[":
+                num+=i
+            
+            elif i=="[":
                 stack.append(num)
                 stack.append(i)
-                num = ""
-
+                num=""
+            
             elif i.isalpha():
                 stack.append(i)
-
-            elif i == "]":
-
-                ss = ""
+            
+            elif i=="]" and stack!=[]:
+                ss=""
                 while stack[-1] != "[":
-                    ss = stack.pop() + ss
-
-                stack.pop() 
-
-                repeat = int(stack.pop())
-                stack.append(ss * repeat)
+                    ss=stack.pop()+ss
+                stack.pop()
+                
+                rep=int(stack.pop())
+                ss=ss*rep
+                stack.append(ss)
 
         return "".join(stack)
