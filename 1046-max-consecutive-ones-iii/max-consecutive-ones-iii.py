efcc61df45c6afa53def8e41acb8bdf7abe5a955
Piyush Madheshiya <piyushsmmadheshiya08@gmail.com>
2026-06-13 15:18:21 +0530

    Time: 48 ms (89.00%) | Memory: 22.1 MB (85.05%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -17,33 +17,33 @@ class Solution:
         zeros=0
         n=len(nums)
         
-        while r<n:
-            if nums[r]==0:
-                zeros+=1
-
-            while zeros>k:
-                if nums[l]==0:
-                    zeros-=1
-                l+=1
-            
-            maxi=max(maxi,r-l+1)
-            r+=1
-        return maxi
-        
-        
         # while r<n:
         #     if nums[r]==0:
         #         zeros+=1
 
-        #     if zeros>k:
+        #     while zeros>k:
         #         if nums[l]==0:
         #             zeros-=1
         #         l+=1
             
-        #     if zeros<=k:
-        #         maxi=max(maxi,r-l+1)
+        #     maxi=max(maxi,r-l+1)
         #     r+=1
         # return maxi
+        
+        
+        while r<n:
+            if nums[r]==0:
+                zeros+=1
+
+            if zeros>k:
+                if nums[l]==0:
+                    zeros-=1
+                l+=1
+            
+            if zeros<=k:
+                maxi=max(maxi,r-l+1)
+            r+=1
+        return maxi
 
         
            
