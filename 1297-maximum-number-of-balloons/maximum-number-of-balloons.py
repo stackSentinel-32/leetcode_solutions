eee15ac0e2a84fce553059cd1570f39285021af7
Piyush Madheshiya <piyushsmmadheshiya08@gmail.com>
2026-06-30 00:30:53 +0530

    Time: 3 ms (56.88%) | Memory: 19.4 MB (37.05%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -0,0 +1,5 @@
+from collections import Counter
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        freq=Counter(text)
+        return min(freq['b'],freq['a'],freq['l']//2,freq['o']//2,freq['n'])
